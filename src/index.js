7c595d5d4573f022212b809c40494a4aece51306
Ilya Kutko <36884482+DyatelVudu@users.noreply.github.com>
2018-03-15 20:27:33 +0300

    almost done 2

--- src/index.js
+++ src/index.js
@@ -32,9 +32,38 @@ const asyncIncrementor = () => {
         return resolve(count2);
     })
 };
-const createIncrementer = () => {};
-const returnBackInSecond = (arg) => {};
-const getDeepPropertiesCount = (obj) => {};
+const createIncrementer = () => {
+    return {
+        i: 0,
+        next() {
+            return { value: ++this.i }
+        },
+        [Symbol.iterator]() {
+            return {
+                next: () => {
+                    return this.next()
+                },
+            };
+        }
+    };
+};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve(param)
+        },1100)
+    })
+};
+const getDeepPropertiesCount = (object) => {
+    var properties = Object.getOwnPropertyNames(object);
+    var count = properties.length;
+    properties.forEach(property => {
+        if (Object.getOwnPropertyNames(object[property]).length > 0){
+            count = count + getDeepPropertiesCount(object[property]);
+        }
+    })
+    return count;
+};
 const createSerializedObject = () => {
     return Object(1);
 };
